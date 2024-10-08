6bbfaf014c4851a3b1c5cc339caea21f068e2950
EkaterinaBordychevskaya <102412682+EkaterinaBordychevskaya@users.noreply.github.com>
2024-10-08 23:15:20 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,10 +1,10 @@
 try:
-    x=int(input("Введите делимое число"))
-    y=int(input("Введите делитель"))
+    x=int(input("Введите делимое число: "))
+    y=int(input("Введите делитель: "))
     result=x/y
 except ZeroDivisionError:
     print("Ошибка! Нельзя делить на ноль.")
 except TypeError:
     print("Ошибка! Введено значение неподходящего типа.")
 else:
-    print("Результат деления {x} на {y} равен {result}")
+    print(f"Результат деления {x} на {y} равен {result}")
